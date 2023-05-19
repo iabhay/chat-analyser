9eccc57347a8cb204c5e192f8ad47ba4bb34fb44
Abhay Agrawal <73734837+iabhay@users.noreply.github.com>
2023-05-19 18:39:45 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -9,7 +9,7 @@ def preprocess(data):
     chk = 0
     if "am" in df['message_date'][0]:
         chk = 1
-     if "pm" in df['message_date'][0]:
+    if "pm" in df['message_date'][0]:
         chk = 1
     df['message_date'].replace(to_replace="am", value="AM")
     df['message_date'].replace(to_replace="pm", value="PM")
