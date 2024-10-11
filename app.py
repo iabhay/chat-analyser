83dd3b243c269d80ed3a879a678d3d839fed7298
Abhay Agrawal <73734837+iabhay@users.noreply.github.com>
2024-10-11 16:19:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ if uploaded_file is not None:
     # st.dataframe(df)
 
     user_list = df['user'].unique().tolist()
-    user_list.remove('group_notification')
+    if 'group_notification' in user_list:
+        user_list.remove('group_notification')
     user_list.sort()
     user_list.insert(0, "Overview")
 
